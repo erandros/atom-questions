26dd013140afa9e2b7f5c1a92cfcb26615c8bb4e
erandros <guidotarsia@gmail.com>
2017-03-06 12:04:24 -0300

    Initial commit

---
+++ spec/atom-questions-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomQuestionsView from '../lib/atom-questions-view';
+
+describe('AtomQuestionsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
